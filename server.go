dbf04e8ce37600a16be47f1017f2af44086a4bd5
Efesto <polita.marco@gmail.com>
2018-12-28 10:50:07 +0100

    interaction with openWeather

--- server.go
+++ server.go
@@ -1,18 +1,41 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
+	"github.com/subosito/gotenv"
 )
 
 func main() {
+	gotenv.Load()
+
 	e := echo.New()
-	e.Logger.Fatal(e.Start(":1323"))
 
 	e.GET("/greetings", greetings)
+	e.GET("/weather/:city", weather)
+
+	e.Logger.Fatal(e.Start(":5000"))
 }
 
 func greetings(context echo.Context) error {
 	return context.String(http.StatusOK, "Hello, World!")
+}
+
+func weather(context echo.Context) error {
+	city := context.Param("city")
+	apiKey := openWeatherAPIKey()
+
+	url := "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	return context.String(http.StatusOK, string(body))
+}
+
+func openWeatherAPIKey() string {
+	return os.Getenv("OPEN_WEATHER_API_KEY")
 }
